8d148f02119499f741a45627c250ba5eadcf0482
FazzaCha <108410949+FazzaCha@users.noreply.github.com>
2022-06-29 23:02:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,45 @@
+provider "aws" {
+  region ="us-east-1"
+  access_key="AKIA5STC2SJ6VJPTDQIP"
+  secret_key="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"
+
+}
+
 resource "aws_s3_bucket" "bucket" {
   acl = "private"
 }
 
-tags = {
-  Name = test_bucket1
-}
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+  bucket = aws_s3_bucket.bucket.bucket
 
-versioning {
-  enabled = var.versioning
-}
+  rule {
+    id = "log"
 
-lifecycl_rule {
-  enabled = var.lifecycle
+    expiration {
+      days = 90
+    }
+
+    filter {
+      and {
+        prefix = "log/"
+
+        tags = {
+          rule      = "log"
+          autoclean = "true"
+        }
+      }
+    }
+
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
 }
